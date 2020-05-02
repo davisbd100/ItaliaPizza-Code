3a83f0188ad288c24cff3bebcaec75e85614db33
jarturogl05 <jarturogl05@gmail.com>
2020-05-01 21:37:12 -0500

    Adición de enumeración para los resultados de los DAO

---
+++ ItaliaPizza/BusinessLogic/AddEnum.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+
+    /// <summary>Clase con una enumeracion para los posibles resultados de una operacion</summary>
+    public class AddEnum
+    {
+        public enum AddResult
+        {
+            Success,
+            NullObject,
+            UnknowFail,
+            SQLFail,
+            ExistingRecord
+        }
+    }
+}
