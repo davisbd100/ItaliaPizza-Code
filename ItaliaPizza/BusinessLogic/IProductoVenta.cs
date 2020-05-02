4b560d3aca7e69c5c8dd794e946953c1a68ecafc
jarturogl05 <jarturogl05@gmail.com>
2020-05-01 22:43:29 -0500

    Interface Correspondientes a productos

---
+++ ItaliaPizza/BusinessLogic/IProductoVenta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BusinessLogic.ResultadoOperacionEnum;
+
+
+namespace BusinessLogic
+{
+    interface IProductoVenta
+    {
+        List<ProductoVenta> GetProductosVenta();
+        List<ProductoVenta> ProductoVentaBusqueda();
+        ProductoVenta ObtenerProductoVentaPorid(string id);
+        ResultadoOperacion AddProductoVenta(ProductoVenta productoVenta);
+        ResultadoOperacion EliminarProductoVenta(ProductoVenta productoVenta);
+        ResultadoOperacion EditarProductoVenta(ProductoVenta productoVenta);
+    }
+}
