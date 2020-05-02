89bbd940019b61edd25dda7a079ab69235ee41db
jarturogl05 <jarturogl05@gmail.com>
2020-05-01 21:59:39 -0500

    Clases referentes a los productos

---
+++ ItaliaPizza/BusinessLogic/ProductoVenta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class ProductoVenta: Producto
+    {
+        public float PreciPúblico { get; set; }
+        public TipoProductoEnum TipoProducto { get; set; }
+        public string FotoProducto { get; set; }
+        public bool TieneReceta { get; set; }
+        public Receta Receta { get; set; }
+
+        public ProductoVenta() { }
+        
+
+
+    }
+
+    public enum TipoProductoEnum
+    {
+
+ 
+    }
+}
