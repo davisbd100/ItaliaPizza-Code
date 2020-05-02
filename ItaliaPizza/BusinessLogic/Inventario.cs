f36d5670bc7cd637e5ae0d63e9aaddcc1ff14ec9
jarturogl05 <jarturogl05@gmail.com>
2020-05-01 22:13:41 -0500

    Clases referentes a el inventario

---
+++ ItaliaPizza/BusinessLogic/Inventario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Inventario
+    {
+        public Producto Producto { get; set; }
+        public int CantidadIngreso { get; set; }
+        public float PrecioCompra { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public DateTime Caducidad { get; set; }
+        public int ExistenciaTotal { get; set; }
+        public UnidadDeMedidaEnum UnidadDeMedida { get; set; }
+
+
+    }
+
+    public enum UnidadDeMedidaEnum
+    {
+        Kilo,
+        Gramo,
+        Litro,
+        Mililitro,
+        Cucharada,
+        Taza,
+        Galón
+    }
+
+}
--- ItaliaPizza/BusinessLogic/ProductoVenta.cs
+++ ItaliaPizza/BusinessLogic/ProductoVenta.cs
@@ -22,7 +22,5 @@ namespace BusinessLogic
 
     public enum TipoProductoEnum
     {
-
- 
     }
 }
