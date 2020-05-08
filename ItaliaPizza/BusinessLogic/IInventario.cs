cae66753e3d1cc3bcb536966ca7f6db870343d90
jarturogl05 <jarturogl05@gmail.com>
2020-05-08 13:31:13 -0500

    Creación del método agregar inventario

--- ItaliaPizza/BusinessLogic/IInventario.cs
+++ ItaliaPizza/BusinessLogic/IInventario.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static BusinessLogic.ResultadoOperacionEnum;
 
 namespace BusinessLogic
 {
     interface IInventario
     {
         List<Inventario> ObtenerInventarios();
+        ResultadoOperacion AddInventario(Inventario inventario);
+
 
     }
 }
