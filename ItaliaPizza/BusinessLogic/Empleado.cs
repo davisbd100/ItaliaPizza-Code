38b03f4956eb9393ef3034e5b82dc8ee9a7b559b
David <davisbd100@gmail.com>
2020-05-13 16:08:08 -0500

    Agregado de clase Empleado

---
+++ ItaliaPizza/BusinessLogic/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Empleado : Persona
+    {
+        public TipoEmpleado tipoEmpleado { get; set; }
+        public String NombreUsuario { get; set; }
+        public String Contraseña { get; set; }
+        public DateTime FechaUltimoAcceso { get; set; }
+
+        public Empleado()
+        {
+
+        }
+    }
+}
