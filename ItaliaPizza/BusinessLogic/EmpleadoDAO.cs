b6842f132e133c339666aa1802276897c9295d00
gaby2327 <gabriela.uv@outlook.com>
2020-05-21 13:00:39 -0500

    Creación de EmpleadoDAO

---
+++ ItaliaPizza/BusinessLogic/EmpleadoDAO.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using DatabaseConnection;
+using static BusinessLogic.ResultadoOperacionEnum;
+
+namespace BusinessLogic
+{
+    public class EmpleadoDAO : IEmpleado
+    {
+        public ResultadoOperacion AgregarEmpleado(Empleado empleado)
+        {
+            const int VALORES_DUPLICADOS = 2601;
+            ResultadoOperacion resultado = ResultadoOperacion.FallaDesconocida;
+            DbConnection dbconnection = new DbConnection();
+
+            using (SqlConnection connection = dbconnection.GetConnection())
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                SqlTransaction transaction;
+                transaction = connection.BeginTransaction("InsertarEmpleado");
+                command.Connection = connection;
+                command.Transaction = transaction;
+
+                try
+                {
+                    command.CommandText =
+                         "INSERT INTO dbo.Persona VALUES (@idPersona, @Nombre, @Apellido, @Telefono, @Email, @Calle, @Numero, @CodigoPostal, @Colonia, @Ciudad)";
+                    command.Parameters.Add(new SqlParameter("@idPersona", empleado.idPersona));
+                    command.Parameters.Add(new SqlParameter("@Nombre", empleado.Nombre));
+                    command.Parameters.Add(new SqlParameter("@Apellido", empleado.Apellido));
+                    command.Parameters.Add(new SqlParameter("@Telefono", empleado.Telefono));
+                    command.Parameters.Add(new SqlParameter("@Email", empleado.Email));
+                    command.Parameters.Add(new SqlParameter("Calle", empleado.Calle));
+                    command.Parameters.Add(new SqlParameter("Numero", empleado.Numero));
+                    command.Parameters.Add(new SqlParameter("CodigoPostal", empleado.CodigoPostal));
+                    command.Parameters.Add(new SqlParameter("Colonia", empleado.Colonia));
+                    command.Parameters.Add(new SqlParameter("Ciudad", empleado.Ciudad));
+                    command.ExecuteNonQuery();
+
+                    command.CommandText =
+                        "INSERT INTO dbo.Empleado VALUES (@idEmpleado, @TipoEmpleado, @NombreUsuario, @Contrasena, @FechaUltimoAcceso";
+                    command.Parameters.Add(new SqlParameter("@idEmpleado", empleado.idPersona));
+                    command.Parameters.Add(new SqlParameter("@TipoEmpleado", empleado.TipoEmpleado));
+                    command.Parameters.Add(new SqlParameter("NombreUsuario", empleado.NombreUsuario));
+                    command.Parameters.Add(new SqlParameter("Contrasena", empleado.Contraseña));
+                    command.Parameters.Add(new SqlParameter("FechaUltimoAcceso", empleado.FechaUltimoAcceso));
+
+                    transaction.Commit();
+                    resultado = ResultadoOperacion.Exito;
+
+                }
+                catch (SqlException e)
+                {
+                    transaction.Rollback();
+
+                    switch (e.Number)
+                    {
+                        case VALORES_DUPLICADOS:
+                            resultado = ResultadoOperacion.ObjetoExistente;
+                            break;
+                        default:
+                            resultado = ResultadoOperacion.FalloSQL;
+                            break;
+                    }
+                }
+            }
+
+            return resultado;
+        }
+
+        public ResultadoOperacion DarDeBajaEmpleado(Empleado empleado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultadoOperacion EditarEmpleado(Empleado empleado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Empleado> GetEmpleados()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Empleado> GetEmpleadosByDireccion(string Direccion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Empleado> GetEmpleadosByNombre(string Nombre)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Empleado> GetEmpleadosByTelefono(string Telefono)
+        {
+            throw new NotImplementedException();
+        }
+    }
+  }
