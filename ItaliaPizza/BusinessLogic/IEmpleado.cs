0f1c13305769338581b57091e342818b27650928
gaby2327 <gabriela.uv@outlook.com>
2020-05-21 13:01:49 -0500

    Creación IEmpleado

---
+++ ItaliaPizza/BusinessLogic/IEmpleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using static BusinessLogic.ResultadoOperacionEnum;
+
+namespace BusinessLogic
+{
+    interface IEmpleado
+    {
+        List<Empleado> GetEmpleados();
+        List<Empleado> GetEmpleadosByNombre(String Nombre);
+        List<Empleado> GetEmpleadosByTelefono(String Telefono);
+        List<Empleado> GetEmpleadosByDireccion(String Direccion);
+        ResultadoOperacion AgregarEmpleado(Empleado empleado);
+        ResultadoOperacion EditarEmpleado(Empleado empleado);
+        ResultadoOperacion DarDeBajaEmpleado(Empleado empleado);
+    }
+}
