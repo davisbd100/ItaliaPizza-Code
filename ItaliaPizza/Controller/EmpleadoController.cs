817de4dfc62f4b9ce856896901824aff0c16cb49
gaby2327 <gabriela.uv@outlook.com>
2020-05-25 00:47:24 -0500

    Modificación EmpleadoController

--- ItaliaPizza/Controller/EmpleadoController.cs
+++ ItaliaPizza/Controller/EmpleadoController.cs
@@ -4,6 +4,7 @@ using static BusinessLogic.ResultadoOperacionEnum;
 
 namespace Controller
 {
+<<<<<<< Updated upstream
     public class EmpleadoController
     {
         public ResultadoOperacion AgregarEmpleado(String nombre, String apellido,
@@ -45,4 +46,46 @@ namespace Controller
             return empleadoDAO.GetEmpleadoByUsername(username);
         }
     }
+=======
+	public class EmpleadoController
+	{
+		public ResultadoOperacion AgregarEmpleado(String nombre, String apellido,
+			String telefono, String email, String ciudad, String calle, String numero,
+			String colonia, String codigoPostal, String usuario, String contrasena, String tipoEmpleado)
+		{
+			ResultadoOperacion resultado = ResultadoOperacion.FallaDesconocida;
+
+			if (GetEmpleadoByUsername(usuario).NombreUsuario == null)
+			{
+				Empleado empleado = new Empleado();
+				empleado.Nombre = nombre;
+				empleado.Apellido = apellido;
+				empleado.Telefono = telefono;
+				empleado.Email = email;
+				empleado.Ciudad = ciudad;
+				empleado.Calle = calle;
+				empleado.Numero = numero;
+				empleado.Colonia = colonia;
+				empleado.CodigoPostal = codigoPostal;
+				empleado.NombreUsuario = usuario;
+				empleado.Contraseña = contrasena;
+				empleado.TipoEmpleado = tipoEmpleado;
+				EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+				resultado = (ResultadoOperacion)empleadoDAO.AgregarEmpleado(empleado);
+			}
+			else
+			{
+				resultado = ResultadoOperacion.ObjetoExistente;
+			}
+
+			return resultado;
+		}
+
+		public Empleado GetEmpleadoByUsername(String username)
+		{
+			EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+			return empleadoDAO.GetEmpleadoByUsername(username);
+		}
+	}
+>>>>>>> Stashed changes
 }
