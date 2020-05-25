1c5033fab8a7f48731efa119f8069ed45ac00452
jarturogl05 <jarturogl05@gmail.com>
2020-05-24 21:04:23 -0500

    Funcionalidad a la pantalla de registrar productos

--- ItaliaPizza/ItaliaPizza/RegistrarProducto.xaml.cs
+++ ItaliaPizza/ItaliaPizza/RegistrarProducto.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Controller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,5 +24,107 @@ namespace ItaliaPizza
         {
             InitializeComponent();
         }
+        private enum CheckResult
+        {
+            Passed,
+            Failed
+        }
+
+        private bool VerificarCamposVacios()
+        {
+            bool resultado = true;
+
+            if (txb_Nombre.Text == String.Empty || txb_Codigo.Text == String.Empty || txb_Descripcion.Text == String.Empty
+                || txb_Preciounitario.Text == String.Empty || txb_Restricción.Text == String.Empty || txb_UnidadMedida.Text == String.Empty
+                || txb_Ubicacion.Text == String.Empty || txb_Cantidad.Text == String.Empty || txb_Caducidad.Text == String.Empty )
+            {
+                resultado = false;
+            }
+
+            return resultado;
+        }
+
+        private bool VerificarCampos()
+        {
+            bool resultado = true;
+            ValidarCampos validarCampos = new ValidarCampos();
+            if (!VerificarCamposVacios())
+            {
+                MessageBox.Show("Existen campos vacios");
+                resultado = false;
+            }
+            else if (validarCampos.ValidarNumeroEntero(txb_Codigo.Text) == ValidarCampos.ResultadosValidación.Numeroinvalido
+                || validarCampos.ValidarNumeroEntero(txb_Cantidad.Text) == ValidarCampos.ResultadosValidación.Numeroinvalido)
+            {
+                MessageBox.Show("Codigo y cantidad deben ser números enteros");
+                resultado = false;
+            }
+            else if (validarCampos.ValidarNumeroFloat(txb_Preciounitario.Text) == ValidarCampos.ResultadosValidación.Numeroinvalido ||
+                validarCampos.ValidarNumeroFloat(txb_PrecioVenta.Text) == ValidarCampos.ResultadosValidación.Numeroinvalido)
+            {
+                MessageBox.Show("Los precios deben ser solo números");
+                resultado = false;
+            }
+
+           return resultado;
+        }
+
+        private int VerificarTipoIngrediente()
+        {
+            int index = Convert.ToInt32(cbb_TipoIngrediente.SelectedIndex.ToString());
+            Console.WriteLine(index);
+
+            if (index == -1)
+            {
+               MessageBox.Show("Debe ingresar el tipo de ingrediente");
+            }
+            return 1;
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int tipoIngrediente = VerificarTipoIngrediente();
+            if (VerificarCampos())
+            {
+                if(tipoIngrediente == 0)
+                {
+
+                }else if (tipoIngrediente == 1)
+                {
+                    RegistrarProductoVenta();
+                }
+
+            }
+        }
+
+        private void RegistrarProductoVenta()
+        {
+            ProductoVentaController productoVentaController = new ProductoVentaController();
+            bool requiereReceta = false;
+            if (cb_RequiereReceta.IsChecked.Value)
+            {
+                requiereReceta = true;
+            }
+
+            if (productoVentaController.crearProducto(txb_Nombre.Text, Convert.ToInt32( txb_Codigo.Text), txb_Descripcion.Text, float.Parse( txb_Preciounitario.Text), 
+                txb_Restricción.Text, txb_UnidadMedida.Text, float.Parse(txb_PrecioVenta.Text),requiereReceta, "url de la foto",txb_Ubicacion.Text, 
+                Convert.ToInt32(txb_Cantidad.Text) ,txb_Caducidad.Text, "Example" ) == BusinessLogic.ResultadoOperacionEnum.ResultadoOperacion.Exito)
+            {
+                MessageBox.Show("Producto registrado con éxito");
+            }
+            else
+            {
+                MessageBox.Show("ocurrió un error");
+            }
+        }
+
+        private void RegistrarProductoingrediente()
+        {
+
+        }
+
+
+
     }
 }
