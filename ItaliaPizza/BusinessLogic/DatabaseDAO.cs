1911f6ca226066b28cd33e822eb7b39ee5095288
David <davisbd100@gmail.com>
2020-05-25 20:28:24 -0500

    Agregado de metodo de obtencion de bd

---
+++ ItaliaPizza/BusinessLogic/DatabaseDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseConnection;
+
+namespace BusinessLogic
+{
+    public class DatabaseDAO
+    {
+        public ResultadoOperacionEnum.ResultadoOperacion ObtenerRespaldo(String ruta)
+        {
+            ResultadoOperacionEnum.ResultadoOperacion resultado = ResultadoOperacionEnum.ResultadoOperacion.FallaDesconocida;
+            DbConnection dbConnection = new DbConnection();
+            string comando = "BACKUP DATABASE [Pizza] TO DISK " + ruta + "\\Pizza-" + DateTime.Now.ToString() + ".bak";
+
+            using (SqlConnection sqlConnection = dbConnection.GetConnection())
+            {
+                sqlConnection.Open();
+
+                try
+                {
+                    using (SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection))
+                    {
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                    resultado = ResultadoOperacionEnum.ResultadoOperacion.Exito;
+                }
+                catch (SqlException)
+                {
+                    resultado = ResultadoOperacionEnum.ResultadoOperacion.FalloSQL;
+                }
+            }
+            return resultado;
+        }
+    }
+}
