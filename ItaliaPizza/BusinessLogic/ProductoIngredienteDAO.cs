005624c8c8cd52052137fbbfbf53f366b214abb9
jarturogl05 <jarturogl05@gmail.com>
2020-05-27 22:40:18 -0500

    Cambio de códigos para productos duplicados

--- ItaliaPizza/BusinessLogic/ProductoIngredienteDAO.cs
+++ ItaliaPizza/BusinessLogic/ProductoIngredienteDAO.cs
@@ -15,6 +15,7 @@ namespace BusinessLogic
         public ResultadoOperacionEnum.ResultadoOperacion AddProductoIngrediente(ProductoIngrediente productoIngrediente, Inventario inventario)
         {
             const int VALORES_DUPLICADOS = 2601;
+            const int VALOR_EXISTENTE = 2627;
             ResultadoOperacion resultado = ResultadoOperacion.FallaDesconocida;
             DbConnection dbConnection = new DbConnection();
 
@@ -71,6 +72,9 @@ namespace BusinessLogic
                     switch (e.Number)
                     {
                         case VALORES_DUPLICADOS:
+                            resultado = ResultadoOperacion.ObjetoExistente;
+                            break;
+                        case VALOR_EXISTENTE:
                             resultado = ResultadoOperacion.ObjetoExistente;
                             break;
                         default:
