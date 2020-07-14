3298a880b77c15d513420edbd39cb59375c8d692
gaby2327 <gabriela.uv@outlook.com>
2020-07-14 10:36:53 -0500

    Modificación ventana cocinero

--- ItaliaPizza/ItaliaPizza/ListaPedidoCocinero.xaml.cs
+++ ItaliaPizza/ItaliaPizza/ListaPedidoCocinero.xaml.cs
@@ -1,18 +1,7 @@
-﻿using BusinessLogic;
-using Controller;
+﻿using Controller;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PrototiposItaliaPizza
 {
@@ -75,6 +64,11 @@ namespace PrototiposItaliaPizza
                 pedidoActual = null;
                 lbidPedidoActual.Content = "Ninguno";
             }
+        }
+
+        private void SalirButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
