b077e1c1235f815cfc8387149a69e6bf0bdd4b4a
davisbd100 <davisbd100@gmail.com>
2020-07-13 21:51:21 -0500

    User control de PedidosCocina

---
+++ ItaliaPizza/ItaliaPizza/Controls/PedidosUC.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Controller;
+using BusinessLogic;
+
+namespace ItaliaPizza.Controls
+{
+    /// <summary>
+    /// Interaction logic for PedidosUC.xaml
+    /// </summary>
+    public partial class PedidosUC : UserControl
+    {
+        public event EventHandler PedidoUserControlClicked;
+        PedidoController Controller = new PedidoController();
+        List<DataAccess.Pedido> Pedidos = new List<DataAccess.Pedido>();
+        public PedidosUC()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Loaded");
+            Pedidos = Controller.ObtenerPedidosCocina();
+        }
+
+
+        private void btContent_Click(object sender, RoutedEventArgs e)
+        {
+            PedidoUserControlClicked?.Invoke(((Button)sender).DataContext, EventArgs.Empty);
+        }
+    }
+}
