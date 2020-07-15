4526b197d9bc901dd4821aa6ec9d3b651ef27c3c
jarturogl05 <jarturogl05@gmail.com>
2020-07-15 17:57:36 -0500

    Registrar recetas con cantidad

--- ItaliaPizza/BusinessLogic/ListaIngredientesReceta.cs
+++ ItaliaPizza/BusinessLogic/ListaIngredientesReceta.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BusinessLogic
 {
-    public class ListaIngredientesReceta
+    public class ListaIngredientesReceta : ProductoIngrediente
     {
         public int IdIngrediente { get; set; }
         public int Cantidad { get; set; }
--- ItaliaPizza/Controller/RecetaController.cs
+++ ItaliaPizza/Controller/RecetaController.cs
@@ -11,27 +11,29 @@ namespace Controller
     public class RecetaController
     {
         public ResultadoOperacion CrearReceta(string nombre, string procedimiento, string rendimiento,
-            List<ProductoIngrediente> productos, int productoVenta)
+            List<ListaIngredientesReceta> productos, int productoVenta)
         {
             ResultadoOperacion resultadoOperacion = new ResultadoOperacion();
 
             List <ListaIngredientesReceta> listaIngredientesReceta = new List<ListaIngredientesReceta>();
 
-            foreach(ProductoIngrediente productoIngrediente in productos)
-            {
-                ListaIngredientesReceta ingredientesReceta = new ListaIngredientesReceta();
-                ingredientesReceta.IdIngrediente = productoIngrediente.idProducto;
-                ingredientesReceta.Cantidad = 1;
-                ingredientesReceta.PrecioUnitario = 0;
-                listaIngredientesReceta.Add(ingredientesReceta);
+            //foreach(ProductoIngrediente productoIngrediente in productos)
+            //{
+            //    ListaIngredientesReceta ingredientesReceta = new ListaIngredientesReceta();
+            //    ingredientesReceta.IdIngrediente = productoIngrediente.idProducto;
+            //    ingredientesReceta.Cantidad = 1;
+            //    ingredientesReceta.PrecioUnitario = 0;
+            //    listaIngredientesReceta.Add(ingredientesReceta);
                 
-            }
+            //}
+
+
 
             Receta receta = new Receta();
             receta.Nombre = nombre;
             receta.Procedimiento = procedimiento;
             receta.Rendimiento = float.Parse(rendimiento);
-            receta.Ingredientes = listaIngredientesReceta;
+            receta.Ingredientes = productos;
 
             RecetaDAO recetaDAO = new RecetaDAO();
             resultadoOperacion = recetaDAO.AddReceta(receta, productoVenta);
