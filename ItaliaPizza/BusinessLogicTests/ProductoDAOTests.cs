ce4337c85bb69e23c0ddcbaf5d995c22a5d5d804
davisbd100 <davisbd100@gmail.com>
2020-07-17 14:26:24 -0500

    Pruebas unitarias de producto

---
+++ ItaliaPizza/BusinessLogicTests/ProductoDAOTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Tests
+{
+    [TestClass()]
+    public class ProductoDAOTests
+    {
+        [TestMethod()]
+        public void GetProductoPorIDTest()
+        {
+            ProductoDAO productoDAO = new ProductoDAO();
+            Assert.AreEqual(productoDAO.GetProductoPorID(1).idProducto, 1);
+        }
+    }
+}
